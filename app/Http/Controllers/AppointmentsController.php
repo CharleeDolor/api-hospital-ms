616dffa68c72ecc47c32ca38a93305ab06420ec1
Charlee_Dolor <dolorcharlee55@gmail.com>
2024-06-09 20:49:40 +0800

    added index & show function

--- app/Http/Controllers/AppointmentsController.php
+++ app/Http/Controllers/AppointmentsController.php
@@ -7,6 +7,90 @@ use App\Models\Appointment;
 
 class AppointmentsController extends Controller
 {
+
+    public function index()
+    {
+        try {
+            // check permissions
+            $permissions = json_decode(auth('sanctum')->user()->getAllPermissions()->pluck('name'));
+            if (in_array('view appointments', $permissions)) {
+
+                $current_user_id = auth('sanctum')->user()->details_id;
+
+                switch (auth('sanctum')->user()->getRoleNames()->first()) {
+                    case "patient":
+                        $appointments = json_decode(Appointment::join('patients', 'appointments.patient_id', '=', 'patients.id')
+                            ->join('doctors', 'appointments.doctor_id', '=', 'doctors.id')
+                            ->select(
+                                'appointments.type as type',
+                                'appointments.queue_number as queue_number',
+                                'appointments.day as day',
+                                'appointments.patient_id as patient_id',
+                                'doctors.name as doctors_name'
+                            )
+                            ->where('patients.id', $current_user_id)
+                            ->getQuery()
+                            ->get(), true);
+                        break;
+
+                    case "doctor":
+                        $appointments = json_decode(Appointment::join('doctors', 'appointments.patient_id', '=', 'doctors.id')
+                            ->join('patients', 'appointments.patient_id', '=', 'patients.id')
+                            ->select(
+                                'appointments.type as type',
+                                'appointments.queue_number as queue_number',
+                                'appointments.day as day',
+                                'appointments.patient_id as patient_id',
+                                'patients.name as patients_name'
+                            )
+                            ->where('doctors.id', $current_user_id)
+                            ->getQuery()
+                            ->get(), true);
+                        break;
+
+                    default:
+                        $appointments = Appointment::all();
+                        break;
+                }
+
+                return response()->json([
+                    'appointments' => $appointments,
+                ], 200);
+            } else {
+                return response()->json([
+                    'message' => 'Forbidden',
+                ], 403);
+            }
+        } catch (\Throwable $th) {
+            return response()->json([
+                'message' => 'Something went wrong. Please try again' . $th,
+            ], 500);
+        }
+    }
+
+    public function show($id)
+    {
+        try {
+            // check permissions
+            $permissions = json_decode(auth('sanctum')->user()->getAllPermissions()->pluck('name'));
+            if (in_array('view appointments', $permissions)) {
+                $appointment = Appointment::where('id', $id)->firstOrFail();
+
+                return response()->json([
+                    'appointment' => $appointment
+                ], 200);
+            } else {
+                return response()->json([
+                    'message' => 'Forbidden',
+                ], 403);
+            }
+        } catch (\Throwable $th) {
+            return response()->json([
+                'message' => 'Something went wrong. Please try again' . $th,
+            ], 500);
+        }
+    }
+
     public function store(Request $request)
     {
         try {
